7d24f8c7a2b09ea9b4253e9b16b123f7cf3de5b4
XeiDaMoKa <xeidamoka@hotmail.com>
2023-08-16 06:08:57 +0100

    Changed the font color of .h4 and .line-h24 to ff5500. Removed the color change for .topSection___U7sVi.

--- Xei ● Torn • Scripts/Better-SideBar.user/Better-SideBar.user.js
+++ Xei ● Torn • Scripts/Better-SideBar.user/Better-SideBar.user.js
@@ -20,7 +20,6 @@
 // change font color of .h4 and .line-h24 to ff5500
     $('#h4 , .line-h24').css('color', '#ff5500 !important');
 
-    $('.topSection___U7sVi ').css('color', '#ff00ff !important');
 
 
 
