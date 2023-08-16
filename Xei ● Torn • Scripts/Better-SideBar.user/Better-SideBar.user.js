ebb604a961cd3a74c92db4955950965e192de071
XeiDaMoKa <xeidamoka@hotmail.com>
2023-08-16 06:30:52 +0100

    Changed the font color of .h4 and .line-h24 to ff5500.

--- Xei ● Torn • Scripts/Better-SideBar.user/Better-SideBar.user.js
+++ Xei ● Torn • Scripts/Better-SideBar.user/Better-SideBar.user.js
@@ -19,9 +19,7 @@
     $('body').css('background-color', '#1b0025 !important');
 
 // change font color of .h4 and .line-h24 to ff5500
-$('h4').css('color', '#ff5500 !important');
-
-$('h4 span').css('color', '#ff00ff !important');
+$('.content-title .m-bottom10').css('color', '#ff5500 !important');
 
 
     //  1º value Title / 2º value Fill Color / 3º value Stroke /  4º value Stroke Color
