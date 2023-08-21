16c3a3e7405893620cb86ee7a42d44cb9c8955b5
XeiDaMoKa <xeidamoka@hotmail.com>
2023-08-21 03:25:54 +0100

    .

--- Xei ● Torn • Extensions/torncustoms/customs.js
+++ Xei ● Torn • Extensions/torncustoms/customs.js
@@ -3,11 +3,6 @@
 
 
 
-// Dynamically apply the body color
-chrome.runtime.onMessage.addListener(function(request) {
-    document.querySelector('body').style.backgroundColor = request.body;
-});
-
 // Apply the body color from Storage
 chrome.storage.local.get("body", function(data) {
     document.querySelector('body').style.backgroundColor = data.body;
--- Xei ● Torn • Extensions/torncustoms/popup.js
+++ Xei ● Torn • Extensions/torncustoms/popup.js
@@ -3,15 +3,24 @@
 
 
 
-    // Listens for changes in background color picker
-    $('.input-color-picker[data-id="bg-color"]').on('input', function() {
-        var newBodyColor = $(this).val();
+// Listens for changes in background color picker
+$('.bodyColorPicker[data-id="bodyColor"]').on('input', function() {
+    var newBodyColor = $(this).val();
 
-        // Sends the color to customs.js
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, {action: "changeBodyColor", body: newBodyColor});
-
-            // Saves in local storage
-            chrome.storage.local.set({"body": newBodyColor});
+    // Dynamically change the body color from the color picker
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        chrome.scripting.executeScript({
+            target: { tabId: tabs[0].id },
+            func: setBackgroundColor,
+            args: [newBodyColor]
         });
+
+        // Save the body color to storage
+        chrome.storage.local.set({ body: newBodyColor });
     });
+});
+
+// Listens for changes in background color picker and applies the color to the body
+function setBackgroundColor(color) {
+    document.body.style.backgroundColor = color;
+}
