7e01043fac6f44f4b2d3b83398e4c7ed7b661284
XeiDaMoKa <xeidamoka@hotmail.com>
2023-08-21 04:18:24 +0100

    upload image feature

--- Xei ● Torn • Extensions/torncustoms/customs.js
+++ Xei ● Torn • Extensions/torncustoms/customs.js
@@ -5,5 +5,10 @@
 
 // Apply the body color from Storage
 chrome.storage.local.get("body", function(data) {
-    document.querySelector('body').style.backgroundColor = data.body;
+    document.body.style.backgroundColor = data.body;
+});
+
+// Apply the background image from Storage
+chrome.storage.local.get("bodyImage", function(data) {
+    document.querySelector('.custom-bg-desktop').style.backgroundImage = `url(${data.bodyImage})`;
 });
--- Xei ● Torn • Extensions/torncustoms/popup.js
+++ Xei ● Torn • Extensions/torncustoms/popup.js
@@ -3,24 +3,48 @@
 
 
 
-// Listens for changes in background color picker
-$('.bodyColorPicker[data-id="bodyColor"]').on('input', function() {
-    var newBodyColor = $(this).val();
+// When color picker value changes
+$('.bodyColorPicker').on('input', function() {
+    // Get the new color value
+    let newBodyColor = $(this).val();
 
-    // Dynamically change the body color from the color picker
+    // Apply the new color to the active tab
+    applyToTab('body', 'backgroundColor', newBodyColor);
+
+    // Save the new color to local storage for persistence
+    saveToStorage('body', newBodyColor);
+});
+
+// Listen for a new image being uploaded
+$('#bodyIMG').on('change', function() {
+    let reader = new FileReader();
+    reader.onloadend = function(event) {
+        // Apply the uploaded image as background to the active tab
+        applyToTab('.custom-bg-desktop', 'backgroundImage', `url(${event.target.result})`);
+
+        // Save the image to local storage for persistence
+        saveToStorage('bodyImage', event.target.result);
+    };
+    reader.readAsDataURL(this.files[0]);
+});
+
+// Apply CSS changes to the tab
+function applyToTab(selector, property, value) {
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
         chrome.scripting.executeScript({
             target: { tabId: tabs[0].id },
-            func: setBackgroundColor,
-            args: [newBodyColor]
+            func: changeStyle,
+            args: [selector, property, value]
         });
-
-        // Save the body color to storage
-        chrome.storage.local.set({ body: newBodyColor });
     });
-});
+}
 
-// Listens for changes in background color picker and applies the color to the body
-function setBackgroundColor(color) {
-    document.body.style.backgroundColor = color;
+// Helper function for changing styles
+function changeStyle(selector, property, value) {
+    document.querySelector(selector).style[property] = value;
+}
+
+// Function to save data to chrome's local storage
+function saveToStorage(key, value) {
+    chrome.storage.local.set({ [key]: value });
 }
