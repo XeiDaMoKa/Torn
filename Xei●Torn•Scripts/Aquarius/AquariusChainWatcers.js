d236064be21821e9caf15ae90c022e9ba1e06a8e
Miguel Lemos <xeidamoka@hotmail.com>
2024-07-06 05:19:18 +0100

    Update AquariusChainWatcers.js

--- Xei●Torn•Scripts/Aquarius/AquariusChainWatcers.js
+++ Xei●Torn•Scripts/Aquarius/AquariusChainWatcers.js
@@ -6,8 +6,8 @@
 // @match        https://www.torn.com/*
 // @homepageURL   https://https://xeidamoka.com/AquariusChainWatchers
 // @homepageURL   https://github.com/XeiDaMoKa/Xei-Torn/blob/Xei/Xei●Torn•Scripts/Aquarius/AquariusChainWatcers.js
-// @downloadURL   https://github.com/XeiDaMoKa/Xei-Torn/edit/Xei/Xei●Torn•Scripts/Aquarius/AquariusChainWatcers.user.js
-// @updateURL     https://github.com/XeiDaMoKa/Xei-Torn/edit/Xei/Xei●Torn•Scripts/Aquarius/AquariusChainWatcers.user.js
+// @downloadURL   https://github.com/XeiDaMoKa/Xei-Torn/raw/Xei/Xei●Torn•Scripts/Aquarius/AquariusChainWatcers.user.js
+// @updateURL     https://github.com/XeiDaMoKa/Xei-Torn/raw/Xei/Xei●Torn•Scripts/Aquarius/AquariusChainWatcers.user.js
 // @supportURL    https://github.com/XeiDaMoKa/Xei-Torn-Aquarius-Scripts/issues
 // @grant        GM_xmlhttpRequest
 // ==/UserScript==
