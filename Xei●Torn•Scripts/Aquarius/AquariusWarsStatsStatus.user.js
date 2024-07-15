6508afb5e0b5a95985bc2ad7554daf4e5e024924
Miguel Lemos <xeidamoka@hotmail.com>
2024-07-16 00:01:00 +0100

    .

--- Xei●Torn•Scripts/Aquarius/AquariusWarsStatsStatus.user.js
+++ Xei●Torn•Scripts/Aquarius/AquariusWarsStatsStatus.user.js
@@ -4,11 +4,11 @@
 // @description  Adds custom stats column from Google Sheet and customizes status text using TornStats API
 // @author       XeiDaMoKa [2373510]
 // @match        https://www.torn.com/factions.php?step*
-// @homepageURL   https://https://xeidamoka.com/AquariusChainWatchers
-// @homepageURL   https://github.com/XeiDaMoKa/Xei-Torn/edit/Xei/Xei%E2%97%8FTorn%E2%80%A2Scripts/Aquarius/AquariusChainWatchers.user.js
-// @downloadURL   https://github.com/XeiDaMoKa/Xei-Torn/raw/Xei/Xei%E2%97%8FTorn%E2%80%A2Scripts/Aquarius/AquariusChainWatchers.user.js
-// @updateURL     https://github.com/XeiDaMoKa/Xei-Torn/raw/Xei/Xei%E2%97%8FTorn%E2%80%A2Scripts/Aquarius/AquariusChainWatchers.user.js
-// @supportURL    https://github.com/XeiDaMoKa/Xei-Torn-Aquarius-Scripts/issues
+// @homepageURL   https://https://xeidamoka.com/AquariusWarStats&Status
+// @homepageURL   https://github.com/XeiDaMoKa/Xei-Torn/blob/Xei/Xei%E2%97%8FTorn%E2%80%A2Scripts/Aquarius/AquariusWarsStatsStatus.user.js
+// @downloadURL   https://github.com/XeiDaMoKa/Xei-Torn/raw/Xei/Xei%E2%97%8FTorn%E2%80%A2Scripts/Aquarius/AquariusWarsStatsStatus.user.js
+// @updateURL     https://github.com/XeiDaMoKa/Xei-Torn/raw/Xei/Xei%E2%97%8FTorn%E2%80%A2Scripts/Aquarius/AquariusWarsStatsStatus.user.js
+// @supportURL    https://github.com/XeiDaMoKa/Xei-Torn/issues
 // @grant        GM.xmlHttpRequest
 // ==/UserScript==
 
